fbf86850ec85b55341cf02837da1f24c25dc2a3a
kreuzer <t.kreuzer@fz-juelich.de>
2020-05-23 13:12:29 +0200

    add GET function for account removal, to get information about user files

--- app/deletion.py
+++ app/deletion.py
@@ -15,6 +15,15 @@ from pathlib import Path
 from contextlib import closing
 import shutil
 
+
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
 class DeletionHandler(Resource):
     def delete(self):
         """
@@ -26,7 +35,7 @@ class DeletionHandler(Resource):
         try:
             # Track actions through different webservices.
             uuidcode = request.headers.get('uuidcode', '<no uuidcode>')
-            app.log.info("uuidcode={} - Delete Server".format(uuidcode))
+            app.log.info("uuidcode={} - Delete Account".format(uuidcode))
             app.log.trace("uuidcode={} - Headers: {}".format(uuidcode, request.headers))
     
             # Check for the J4J intern token
@@ -112,6 +121,65 @@ class DeletionHandler(Resource):
                 app.log.exception("uuidcode={} - Could not delete the users directories".format(uuidcode))
             
         except:
-            app.log.exception("JLabs.delete failed. Bugfix required")
+            app.log.exception("Deletion.delete failed. Bugfix required")
             return "", 500
         return '', 204
+
+
+    def get(self):
+        """
+        Headers
+            Intern-Authorization
+            uuidcode
+            email
+        """
+        try:
+            # Track actions through different webservices.
+            uuidcode = request.headers.get('uuidcode', '<no uuidcode>')
+            app.log.info("uuidcode={} - Get Deletion information".format(uuidcode))
+            app.log.trace("uuidcode={} - Headers: {}".format(uuidcode, request.headers))
+    
+            # Check for the J4J intern token
+            utils_common.validate_auth(app.log,
+                                       uuidcode,
+                                       request.headers.get('intern-authorization', None))
+            request_headers = {}
+            for key, value in request.headers.items():
+                if 'Token' in key: # refresh, jhub, access
+                    key = key.replace('-', '_')
+                request_headers[key.lower()] = value           
+            
+            config = utils_file_loads.get_general_config()
+            email = request_headers.get('email', '<no_email_submitted>')
+            email = email.replace("@", "_at_")
+            basefolder = config.get('basefolder', '<no basefolder defined>')
+            userfolder = os.path.join(basefolder, email)
+            workfolder = os.path.join(userfolder, "work")
+            projectfolder = os.path.join(userfolder, "Projects", "MyProjects")
+            totalfiles = 0
+            totalsize = 0
+            for root, dirs, files in os.walk(workfolder):  # @UnusedVariable
+                if root.endswith(".hpc_mount") or \
+                root.endswith(".hpc_mount/.upload") or \
+                root.endswith(".davfs2") or \
+                root.endswith(".davfs2/cache") or \
+                root.endswith(".davfs2/cache/b2drop.eudat.eu-remote.php-webdav+home-jovyan-B2DROP+jovyan") or \
+                root.endswith(".davfs2/certs") or \
+                root.endswith(".davfs2/certs/private") or \
+                root.endswith(".ipynb_checkpoints"):
+                    continue
+                else:
+                    totalfiles += len(files)
+                for file in files:
+                    filepath = os.path.join(root, file)
+                    totalsize += os.path.getsize(filepath)
+            for root, dirs, files in os.walk(projectfolder):  # @UnusedVariable
+                totalfiles += len(files)
+                for file in files:
+                    filepath = os.path.join(root, file)
+                    totalsize += os.path.getsize(filepath)
+            sizeof_fmt(totalsize)
+        except:
+            app.log.exception("Deletion.get failed. Bugfix required")
+            return "", 500
+        return '{}:{}'.format(totalfiles, totalsize), 200
