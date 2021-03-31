776361a02184c38575973c0a716e68e98e2afc89
kreuzer <t.kreuzer@fz-juelich.de>
2021-03-31 15:30:57 +0200

    update images

--- userlabs-mgr/images/2020.2.6.2/bin/start.sh
+++ userlabs-mgr/images/2020.2.6.2/bin/start.sh
@@ -9,35 +9,26 @@ unset ${!EB@}
 if [[ ! -d /home/jovyan/.cache/black/19.3b0 ]]; then
     mkdir -p /home/jovyan/.cache/black/19.3b0
 fi
-TMPDIR="/home/jovyan/.tmp"
+export TMPDIR="/home/jovyan/.tmp"
 if [[ ! -d $TMPDIR ]]; then
     mkdir -p ${TMPDIR}
 fi
 
-export JUPYTERHUB_ACTIVITY_INTERVAL=20
+export JUPYTERHUB_ACTIVITY_INTERVAL=60
 
 
 source /opt/apps/lmod/lmod/init/profile
 export MODULEPATH=/p/software/hdfcloud/stages/2020/modules/all/Compiler/GCCcore/9.3.0:/p/software/hdfcloud/stages/2020/UI/Compilers:/p/software/hdfcloud/stages/2020/UI/Tools:/p/software/hdfcloud/stages/2020/UI
-module load JupyterCollection/2020.2.6
-
-export NOTEBOOK="2020.2.6"
-#source /p/software/hdfcloud/venv/bin/activate
-
-#/opt/apps/bin/bin/mount-judac
-#/opt/apps/bin/bin/automount-B2DROP
+export OTHERSTAGES=/p/software/hdfcloud/otherstages
+module purge && module use $OTHERSTAGES && module load JupyterCollection/2020.2.6
 
 # Inform Jupyter-JSC about the hostname and port
-#curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 60, "failed": false, "message": "", "html_message": "Start Service on '"$SERVICE_NAME:$PORT"'"}' http://jupyterhub-service.jupyterjsc.svc.cluster.local:8001${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
-
 curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 60, "failed": false, "message": "", "html_message": "Start Service on '"$SERVICE_NAME:$PORT"'"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}/hub/api/tunneling/${JUPYTERHUB_USER}/${SERVERNAMESHORT}/${STARTUUIDCODE}/${SERVICE_NAME}/${PORT} &> /dev/null
 
 if [[ ! $? -eq 0 ]]; then
     echo "Could not notify JupyterHub. Send Cancel with official URL."
-    # curl to jupyter-jsc.fz-juelich.de ....
-    # curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"error": "Could not reach JupyterLab."}' http://jupyterhub-service.jupyterjsc.svc.cluster.local:8001${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_CANCEL_URL}
+    curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"error": "Could not reach JupyterLab."}' https://jupyter-jsc-devel.fz-juelich.de${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_CANCEL_URL}
     exit 0
 fi
-
 
 jupyterhub-singleuser --allow-root --config /opt/apps/bin/config.py
--- userlabs-mgr/images/aiidalab/bin/start.sh
+++ userlabs-mgr/images/aiidalab/bin/start.sh
@@ -6,25 +6,13 @@ unset ${!KUBERNETES@}
 unset ${!EB@}
 
 # module load stuff
-if [[ ! -d /home/jovyan/.cache/black/19.3b0 ]]; then
-    mkdir -p /home/jovyan/.cache/black/19.3b0
-fi
-export TMPDIR="/home/jovyan/.tmp"
+export TMPDIR="/home/aiida/.tmp"
 if [[ ! -d $TMPDIR ]]; then
     mkdir -p ${TMPDIR}
 fi
 
 export JUPYTERHUB_ACTIVITY_INTERVAL=60
 
-source /opt/apps/lmod/lmod/init/profile
-export MODULEPATH=/p/software/hdfcloud/stages/2020/modules/all/Compiler/GCCcore/9.3.0:/p/software/hdfcloud/stages/2020/UI/Compilers:/p/software/hdfcloud/stages/2020/UI/Tools:/p/software/hdfcloud/stages/2020/UI
-export OTHERSTAGES=/p/software/hdfcloud/otherstages
-module purge && module use $OTHERSTAGES && module load JupyterCollection/2020.2.6
-
-#/opt/apps/bin/bin/mount-judac
-#/opt/apps/bin/bin/automount-B2DROP
-
-# Inform Jupyter-JSC about the hostname and port
 curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 60, "failed": false, "message": "", "html_message": "Start Service on '"$SERVICE_NAME:$PORT"'"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}/hub/api/tunneling/${JUPYTERHUB_USER}/${SERVERNAMESHORT}/${STARTUUIDCODE}/${SERVICE_NAME}/${PORT} &> /dev/null
 
 if [[ ! $? -eq 0 ]]; then
@@ -33,5 +21,4 @@ if [[ ! $? -eq 0 ]]; then
     exit 0
 fi
 
-
-jupyterhub-singleuser --allow-root --config /opt/apps/bin/config.py
+/sbin/my_my_init
