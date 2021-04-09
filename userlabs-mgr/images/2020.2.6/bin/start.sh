2f245e2bd3039b099ad3263952dca95a8be752a2
kreuzer <t.kreuzer@fz-juelich.de>
2021-04-09 13:36:19 +0200

    update start script for default image

--- userlabs-mgr/images/2020.2.6/bin/start.sh
+++ userlabs-mgr/images/2020.2.6/bin/start.sh
@@ -14,19 +14,8 @@ if [[ ! -d $TMPDIR ]]; then
     mkdir -p ${TMPDIR}
 fi
 
-export JUPYTERHUB_ACTIVITY_INTERVAL=60
-
-source /opt/apps/lmod/lmod/init/profile
-export MODULEPATH=/p/software/hdfcloud/stages/2020/modules/all/Compiler/GCCcore/9.3.0:/p/software/hdfcloud/stages/2020/UI/Compilers:/p/software/hdfcloud/stages/2020/UI/Tools:/p/software/hdfcloud/stages/2020/UI
-export OTHERSTAGES=/p/software/hdfcloud/otherstages
-module purge && module use $OTHERSTAGES && module load JupyterCollection/2020.2.6
-
-#/opt/apps/bin/bin/mount-judac
-#/opt/apps/bin/bin/automount-B2DROP
-
 # Inform Jupyter-JSC about the hostname and port
-curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 60, "failed": false, "message": "", "html_message": "Start Service on '"$SERVICE_NAME:$PORT"'"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}/hub/api/tunneling/${JUPYTERHUB_USER}/${SERVERNAMESHORT}/${STARTUUIDCODE}/${SERVICE_NAME}/${PORT} &> /dev/null
-
+curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 80, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... port-forwarding established"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/tunneling/${JUPYTERHUB_USER}/${SERVERNAMESHORT}/${STARTUUIDCODE}/${SERVICE_NAME}/${PORT} &> /dev/null
 if [[ ! $? -eq 0 ]]; then
     echo "Could not notify JupyterHub. Send Cancel with official URL."
     curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"error": "Could not reach JupyterLab."}' https://jupyter-jsc-devel.fz-juelich.de${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_CANCEL_URL}
@@ -34,4 +23,17 @@ if [[ ! $? -eq 0 ]]; then
 fi
 
 
+export JUPYTERHUB_ACTIVITY_INTERVAL=60
+
+source /opt/apps/lmod/lmod/init/profile
+export MODULEPATH=/p/software/hdfcloud/stages/2020/modules/all/Compiler/GCCcore/9.3.0:/p/software/hdfcloud/stages/2020/UI/Compilers:/p/software/hdfcloud/stages/2020/UI/Tools:/p/software/hdfcloud/stages/2020/UI
+export OTHERSTAGES=/p/software/hdfcloud/otherstages
+module purge && module use $OTHERSTAGES && module load JupyterCollection/2020.2.6
+
+curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 85, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... modules loaded for JupyterCollection/2020.2.6"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
+#/opt/apps/bin/bin/mount-judac
+#/opt/apps/bin/bin/automount-B2DROP
+
+
+curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 90, "failed": false, "message": "", "html_message": "Starting JupyterLab. Waiting for an answer. This may take a few seconds."}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
 jupyterhub-singleuser --allow-root --config /opt/apps/bin/config.py
--- userlabs-mgr/images/2020.2.6/scripts/start.sh
+++ userlabs-mgr/images/2020.2.6/scripts/start.sh
@@ -13,10 +13,12 @@ SERVICE_NAME="jupyterlab-${ID}-service"
 cp -r ${BASE_CONFIG} ${JOB_PATH}
 mkdir -p ${JOB_PATH}/logs
 
+echo "$(date) - Start" >> ${JOB_PATH}/logs/start.log 2>&1
+
 # Load VO specific variables
 TMP=$(curl -X "GET" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" http://${REMOTE_NODE}:${REMOTE_HUB_PORT}/hub/api/user 2>/dev/null)
 VO=$(python3 -c 'import json,sys; d = json.loads(sys.argv[1]); out = d.get("auth_state", {}).get("vo_active", ""); print(out);' "$TMP")
-curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 40, "failed": false, "message": "", "html_message": "Start with VO: '"$VO"'"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}/hub/api/${JUPYTERHUB_STATUS_URL} >> ${JOB_PATH}/logs/start.log 2>&1
+curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 30, "failed": false, "message": "", "html_message": "Request submitted to HDF-Cloud."}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}/hub/api/${JUPYTERHUB_STATUS_URL} >> ${JOB_PATH}/logs/start.log 2>&1
 if [[ ! -f ${BASE_CONFIG}/VOs/${VO}.env ]]; then
 	echo "VO specific configuration for ${VO} does not exist. Use default one" >> ${JOB_PATH}/logs/start.log 2>&1
 	export $(grep -v '^#' ${BASE_CONFIG}/VOs/default.env | xargs) >> ${JOB_PATH}/logs/start.log 2>&1
@@ -32,9 +34,10 @@ if [[ $EC -eq 0 ]]; then
 	kubectl -n ${NAMESPACE} delete configmap userlab-${ID}-cm >> ${JOB_PATH}/logs/start.log 2>&1
 fi
 
+curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 60, "failed": false, "message": "", "html_message": "Preparing environment for your container on HDF-Cloud ..."}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
 JUPYTERHUB_ACTIVITY_URL=http://${REMOTE_NODE}:${REMOTE_HUB_PORT}/hub/api/users/${JUPYTERHUB_USER}/activity
 echo "Create ConfigMap" >> ${JOB_PATH}/logs/start.log 2>&1
-kubectl -n ${NAMESPACE} create configmap userlab-${ID}-cm --from-literal=JUPYTERHUB_ACTIVITY_URL=${JUPYTERHUB_ACTIVITY_URL} --from-literal=JUPYTERHUB_CANCEL_URL=${JUPYTERHUB_CANCEL_URL} --from-literal=JUPYTERHUB_API_URL=${JUPYTERHUB_API_URL} --from-literal=JUPYTERHUB_CLIENT_ID=${JUPYTERHUB_CLIENT_ID} --from-literal=JUPYTERHUB_USER=${JUPYTERHUB_USER} --from-literal=JUPYTERHUB_SERVICE_PREFIX=${JUPYTERHUB_SERVICE_PREFIX} --from-literal=JUPYTERHUB_BASE_URL=${JUPYTERHUB_BASE_URL} --from-literal=PORT=${PORT} --from-literal=STARTUUIDCODE=${STARTUUIDCODE} --from-literal=SERVERNAMESHORT=${SERVERNAMESHORT} --from-literal=REMOTE_HUB_PORT=${REMOTE_HUB_PORT} --from-literal=REMOTE_NODE=${REMOTE_NODE} --from-literal=SERVICE_NAME=${SERVICE_NAME} >> ${JOB_PATH}/logs/start.log 2>&1
+kubectl -n ${NAMESPACE} create configmap userlab-${ID}-cm --from-literal=JUPYTERHUB_STATUS_URL=${JUPYTERHUB_STATUS_URL} --from-literal=JUPYTERHUB_ACTIVITY_URL=${JUPYTERHUB_ACTIVITY_URL} --from-literal=JUPYTERHUB_CANCEL_URL=${JUPYTERHUB_CANCEL_URL} --from-literal=JUPYTERHUB_API_URL=${JUPYTERHUB_API_URL} --from-literal=JUPYTERHUB_CLIENT_ID=${JUPYTERHUB_CLIENT_ID} --from-literal=JUPYTERHUB_USER=${JUPYTERHUB_USER} --from-literal=JUPYTERHUB_SERVICE_PREFIX=${JUPYTERHUB_SERVICE_PREFIX} --from-literal=JUPYTERHUB_BASE_URL=${JUPYTERHUB_BASE_URL} --from-literal=PORT=${PORT} --from-literal=STARTUUIDCODE=${STARTUUIDCODE} --from-literal=SERVERNAMESHORT=${SERVERNAMESHORT} --from-literal=REMOTE_HUB_PORT=${REMOTE_HUB_PORT} --from-literal=REMOTE_NODE=${REMOTE_NODE} --from-literal=SERVICE_NAME=${SERVICE_NAME} >> ${JOB_PATH}/logs/start.log 2>&1
 if [[ ! $? -eq 0 ]]; then
     echo "Could not create ConfigMap. Exit Script" >> ${JOB_PATH}/logs/start.log 2>&1
     exit 1
@@ -55,7 +58,12 @@ fi
 
 # Create userhome directory/volume if not already existing
 LOCAL_USERHOMES_PATH="/mnt/userdata"
+OLD_USERHOMES_PATH="/mnt/userdata/old"
+# /mnt/userdata/old/t.kreuzer_at_fz-juelich.de/work
+# /mnt/userdata/old/t.kreuzer_at_fz-juelich.de/Projects
+
 if [[ ! -d ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID} ]]; then
+    curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 65, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... create persistent home directory for '"${JUPYTERHUB_USER}"'"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
     mkdir ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID} >> ${JOB_PATH}/logs/start.log 2>&1
     cp ${LOCAL_USERHOMES_PATH}/skel/.profile ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/. >> ${JOB_PATH}/logs/start.log 2>&1
     cp ${LOCAL_USERHOMES_PATH}/skel/.bashrc ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/. >> ${JOB_PATH}/logs/start.log 2>&1
@@ -66,9 +74,27 @@ if [[ ! -d ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID} ]]; then
     chmod 700 -R ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/.davfs2 >> ${JOB_PATH}/logs/start.log 2>&1
     chmod 600 ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/.davfs2/secrets >> ${JOB_PATH}/logs/start.log 2>&1
     if [[ ! -d ${JOB_PATH}/bin/motd.d ]]; then
-      mkdir -p ${JOB_PATH}/bin/motd.d >> ${JOB_PATH}/logs/start.log 2>&1
+        mkdir -p ${JOB_PATH}/bin/motd.d >> ${JOB_PATH}/logs/start.log 2>&1
     fi
-    cp -r ${LOCAL_USERHOMES_PATH}/skel/motd.d/mount-b2drop ${JOB_PATH}/bin/motd.d/mount-b2drop >> ${JOB_PATH}/logs/start.log 2>&1
+    if [[ -f ${LOCAL_USERHOMES_PATH}/skel/motd.d/newuser ]]; then
+        cp -r ${LOCAL_USERHOMES_PATH}/skel/motd.d/newuser ${JOB_PATH}/bin/motd.d/newuser >> ${JOB_PATH}/logs/start.log 2>&1
+    fi
+    # cp -r ${LOCAL_USERHOMES_PATH}/skel/motd.d/mount-b2drop ${JOB_PATH}/bin/motd.d/mount-b2drop >> ${JOB_PATH}/logs/start.log 2>&1
+    if [[ -d ${OLD_USERHOMES_PATH}/${JUPYTERHUB_USER/@/_at_} ]]; then
+        echo "Move old files to new path" >> ${JOB_PATH}/logs/start.log 2>&1
+        curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 68, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... copy files from previous HDF-Cloud implementation to new one."}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
+        mkdir -p ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/previous_implementation >> ${JOB_PATH}/logs/start.log 2>&1
+        if [[ -d ${OLD_USERHOMES_PATH}/${JUPYTERHUB_USER/@/_at_}/work ]]; then
+            mv ${OLD_USERHOMES_PATH}/${JUPYTERHUB_USER/@/_at_}/work ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/previous_implementation/. >> ${JOB_PATH}/logs/start.log 2>&1
+        fi
+        if [[ -d ${OLD_USERHOMES_PATH}/${JUPYTERHUB_USER/@/_at_}/Projects/MyProjects ]]; then
+            OLD_PROJECTS=$(ls -l ${OLD_USERHOMES_PATH}/${JUPYTERHUB_USER/@/_at_}/Projects/MyProjects | wc -l)
+            if [[ $OLD_PROJECTS -gt 1 ]]; then
+                mv ${OLD_USERHOMES_PATH}/${JUPYTERHUB_USER/@/_at_}/Projects/MyProjects ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/previous_implementation/. >> ${JOB_PATH}/logs/start.log 2>&1
+            fi
+        fi
+        chown -R 1094:100 ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID}/previous_implementation >> ${JOB_PATH}/logs/start.log 2>&1
+    fi
 else
     # Check Quota of UserData
     USED_HUMAN=$(du -hs ${LOCAL_USERHOMES_PATH}/${JUPYTERHUB_USER_ID} 2> /dev/null | cut -f1)
@@ -89,6 +115,9 @@ else
             cp ${LOCAL_USERHOMES_PATH}/skel/motd.d/softlimit ${JOB_PATH}/bin/motd.d/softlimit >> ${JOB_PATH}/logs/start.log 2>&1
 	    sed -i -e "s/_used_human_/${USED_HUMAN}/g" -e "s/_softlimit_/${STORAGE_USERDATA_SOFT}/g" -e "s/_hardlimit_/${STORAGE_USERDATA_HARD}/g" ${JOB_PATH}/bin/motd.d/softlimit >> ${JOB_PATH}/logs/start.log 2>&1
         fi
+        curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 65, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... disk quota checked. ('"${USED_HUMAN}"'/'"${STORAGE_USERDATA_SOFT}"'). If you reach '"${STORAGE_USERDATA_HARD}"' you cannot start a JupyterLab anymore."}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
+    else
+        curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 65, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... disk quota checked. ('"${USED_HUMAN}"'/'"${STORAGE_USERDATA_SOFT}"')"}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
     fi
 fi
 if [[ -f ${LOCAL_USERHOMES_PATH}/skel/motd ]]; then
@@ -110,6 +139,7 @@ sed -i -e "s/_resource_limit_storage_/${RESOURCE_LIMIT_STORAGE}/g" ${JOB_PATH}/y
 
 # Storage replacement
 sed -i -e "s/_servername_/${SERVERNAMESHORT}/g" ${JOB_PATH}/bin/config.py >> ${JOB_PATH}/logs/start.log 2>&1
+curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 70, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... request-specific configuration generated."}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
 
 kubectl -n ${NAMESPACE} get deployment userlab-${ID} &> /dev/null
 EC=$?
@@ -118,6 +148,7 @@ if [[ $EC -eq 0 ]]; then
 	kubectl -n ${NAMESPACE} delete -f ${JOB_PATH}/yaml/userlab.yaml >> ${JOB_PATH}/logs/start.log 2>&1
 fi
 echo "Create Deployment" >> ${JOB_PATH}/logs/start.log 2>&1
+curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${STARTUUIDCODE}" -H "Content-Type: application/json" --data '{"progress": 75, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... start request-specific container for JupyterLab."}' http://${REMOTE_NODE}:${REMOTE_HUB_PORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_STATUS_URL}
 kubectl -n ${NAMESPACE} apply -f ${JOB_PATH}/yaml >> ${JOB_PATH}/logs/start.log 2>&1
 if [[ ! $? -eq 0 ]]; then
     echo "Could not create Deployment. Exit Script" >> ${JOB_PATH}/logs/start.log 2>&1
