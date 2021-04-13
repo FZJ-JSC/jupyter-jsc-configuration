46cbb0f126d0a1527935e06249c60ded49ef0ae8
kreuzer <t.kreuzer@fz-juelich.de>
2021-04-13 12:18:25 +0200

    add maintenance configuration for maint instance

---
+++ jupyterhub-maintenance/config/common/jupyterhub_config.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import sys
+
+c = get_config()
+c.JupyterHub.log_level = logging.DEBUG
+c.JupyterHub.log_format = "%(asctime)s,%(msecs).03d, Levelno=%(levelno)s, Level=%(levelname)s, Logger=%(name)s, File=%(filename)s, Line=%(lineno)d, %(message)s"
+
+# c.JupyterHub.last_activity_interval = 60
+c.JupyterHub.hub_ip = "0.0.0.0"
+c.JupyterHub.hub_port = 8001
+c.JupyterHub.port = 8000
+
+hcip = os.environ.get("JUPYTERHUB_CONNECT_IP")
+c.JupyterHub.hub_connect_ip = f"{hcip}"
+
+# c.JupyterHub.template_paths = ["/mnt/data/templates"]
+c.JupyterHub.data_files_path = os.environ.get("DATA_FILES_PATH")
+c.JupyterHub.default_url = "/hub/home"
+c.JupyterHub.jinja_environment_options = {
+    "extensions": [
+        "jinja2.ext.do",
+        "jinja2.ext.loopcontrols",
+        "jinja2_ansible_filters.AnsibleCoreFiltersExtension",
+    ]
+}
+
+import uuid
+
+c.JupyterHub.cookie_secret_file = "{}".format(uuid.uuid4().hex)
