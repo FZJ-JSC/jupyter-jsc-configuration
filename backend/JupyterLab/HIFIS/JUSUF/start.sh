3616239d388cf88915f0a588a3939015c6a70519
kreuzer <t.kreuzer@fz-juelich.de>
2021-04-15 13:39:17 +0200

    config fix for hpc startup

--- backend/JupyterLab/HIFIS/JUSUF/start.sh
+++ backend/JupyterLab/HIFIS/JUSUF/start.sh
@@ -62,7 +62,6 @@ if [[ $? -ne 0 ]]; then
 fi
 
 sed -i -e "s|_port_|${JUPYTER_JSC_PORT}|g" -e "s|_home_|${JUPYTER_JSC_HOME}|g" -e "s|_servername_|${JUPYTERHUB_SERVER_NAME}|g" -e "s|_username_|${JUPYTERHUB_USER}|g" -e "s|_remotenode_|${JUPYTER_JSC_REMOTENODE}|g" -e "s|_remoteport_|${JUPYTER_JSC_REMOTEPORT}|g" ${DIR}/.config.py
-cat $EBROOTJUPYTER/etc/jupyter/jupyter_notebook_config.py >> ${DIR}/.config.py
 curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${JUPYTER_JSC_STARTUUID}" -H "Content-Type: application/json" --data '{"progress": 70, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... request-specific configuration generated"}' http://${JUPYTER_JSC_REMOTENODE}:${JUPYTER_JSC_REMOTEPORT}/hub/api/${JUPYTERHUB_STATUS_URL} &> /dev/null
 
 # Quota Check:
@@ -73,8 +72,8 @@ if [[ ! -f ${HOME}/.${JUPYTER_JSC_STARTUUID} ]]; then
     EC2=$?
     if [[ $EC1 -ne 0 || $EC1 -ne 0 ]]; then
         curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${JUPYTER_JSC_STARTUUID}" -H "Content-Type: application/json" --data '{"error": "Disk quota exceeded in $HOME. You have to clean up your home directory before you can start a JupyterLab.", "detail_error": "Jupyter-JSC tried to create a testfile in ${HOME} and failed. Job directory may contain further information: '"${DIR}"'"}' http://${JUPYTER_JSC_REMOTENODE}:${JUPYTER_JSC_REMOTEPORT}${JUPYTERHUB_BASE_URL}hub/api/${JUPYTERHUB_CANCEL_URL} &> /dev/null
-      rm ${HOME}/.${JUPYTER_JSC_STARTUUID}
-      exit 0
+        rm ${HOME}/.${JUPYTER_JSC_STARTUUID}
+        exit 0
     fi
     rm ${HOME}/.${JUPYTER_JSC_STARTUUID}
     curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${JUPYTER_JSC_STARTUUID}" -H "Content-Type: application/json" --data '{"progress": 75, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... disk quota in $HOME checked"}' http://${JUPYTER_JSC_REMOTENODE}:${JUPYTER_JSC_REMOTEPORT}/hub/api/${JUPYTERHUB_STATUS_URL} &> /dev/null
@@ -97,6 +96,9 @@ else
     module load GCCcore/.9.3.0
     module load JupyterCollection/2020.2.6
     curl -X "POST" -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" -H "uuidcode: ${JUPYTER_JSC_STARTUUID}" -H "Content-Type: application/json" --data '{"progress": 80, "failed": false, "message": "", "html_message": "&nbsp;&nbsp;... modules loaded for JupyterCollection/2020.2.6"}' http://${JUPYTER_JSC_REMOTENODE}:${JUPYTER_JSC_REMOTEPORT}/hub/api/${JUPYTERHUB_STATUS_URL} &> /dev/null
+fi
+if [[ -f ${EBROOTJUPYTER}/etc/jupyter/jupyter_notebook_config.py ]]; then
+    cat ${EBROOTJUPYTER}/etc/jupyter/jupyter_notebook_config.py >> ${DIR}/.config.py
 fi
 
 if [[ ! $JUPYTER_JSC_HOME == $HOME ]]; then
