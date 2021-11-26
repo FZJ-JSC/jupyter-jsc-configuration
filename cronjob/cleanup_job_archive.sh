34b9f034d51a918cadadd4a7b2e20a44d26a039e
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2021-11-26 13:26:29 +0000

    delete old userlabs-jobs, too

--- cronjob/cleanup_job_archive.sh
+++ cronjob/cleanup_job_archive.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo find /nfs/jupyter-jsc-live/common/jobs-archive/ -maxdepth 1 -type d -ctime +30 -exec rm -rf {} \;
+sudo find /nfs/jupyter-jsc-live/common/userlabs-jobs/ -maxdepth 1 -type d -ctime +30 -exec rm -rf {} \;
