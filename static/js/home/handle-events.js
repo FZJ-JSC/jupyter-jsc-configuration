2d2764deb9ba268e4c7f9ac8c80a63d3808cf576
Maria Petrova <m.petrova@fz-juelich.de>
2024-04-18 17:53:57 +0200

    Bug fixes

--- static/js/home/handle-events.js
+++ static/js/home/handle-events.js
@@ -110,33 +110,26 @@ require(["jquery", "home/utils", "home/dropdown-options"], function (
     const serviceInfo = getServiceInfo();
     const userInputInfo = (serviceInfo.JupyterLab.options[values.service] || {}).userInput || {};
     var customImageInput = $(`input#${id}-image-input`);
-    var registryAuths = $(`input#${id}-image-private-url-input, input#${id}-image-private-user-input, input#${id}-image-private-pass-input`);
-    // var privateImageUsrInput = $(`input#${id}-image-private-user-input`);
-    // var privateImagePassInput = $(`input#${id}-image-private-pass-input`);
     var customMountInput = $(`#${id}-image-mount-input`);
-    var privateImgInputDivs = $(`#${id}-image-private-url-input-div,#${id}-image-private-user-input-div, #${id}-image-private-pass-input-div`)
+    var registryAuthsInputs = $(`#${id}-image-private-url-input, #${id}-image-private-user-input, #${id}-image-private-pass-input`);
+
+    var registryAuthsInputDivs = $(`#${id}-image-private-url-input-div,#${id}-image-private-user-input-div, #${id}-image-private-pass-input-div`)
     var allUserInputDivs = $(`#${id}-image-input-div, #${id}-image-private-cb-input-div, #${id}-image-mount-cb-input-div, #${id}-image-mount-input-div`)
 
     var inputRequired = userInputInfo.required || false;
     if (!inputRequired) {
       dropdowns.resetInputElement(customImageInput, false);
-      // dropdowns.resetInputElement(privateImageUsrInput, false);
-      // dropdowns.resetInputElement(privateImagePassInput, false);
       dropdowns.resetInputElement(customMountInput, false);
-      registryAuths.forEach(element => {
-        dropdowns.resetInputElement(element, false);
-      });
-      privateImgInputDivs.hide();
+      dropdowns.resetInputElement(registryAuthsInputs, false);
+
+      registryAuthsInputDivs.hide();
       allUserInputDivs.hide();
     } else {
       dropdowns.resetInputElement(customImageInput, true);
-      // dropdowns.resetInputElement(privateImageUsrInput, true);
-      // dropdowns.resetInputElement(privateImagePassInput, true);
+
       // Set default values for the private docker registry authentications to false => fields are hidden and not required
       $(`#${id}-image-private-cb-input`)[0].checked = false;
-      registryAuths.forEach(element => {
-        dropdowns.resetInputElement(element, false);
-      });
+      dropdowns.resetInputElement(registryAuthsInputs, false);
       allUserInputDivs.show();
 
       // Enable user data mount by default
