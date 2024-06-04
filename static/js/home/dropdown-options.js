fab2627e7e522ac2f0cb19c9e4386da33c02b64b
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2024-06-04 21:21:04 +0200

    Revert "Revert "deprecated no longer selectable for new jupyterlabs""
    This reverts commit 4321e5f6eecea77a75f0f764ecc4534c8f773f31.

--- static/js/home/dropdown-options.js
+++ static/js/home/dropdown-options.js
@@ -14,9 +14,14 @@ define(["jquery", "home/utils"], function (
     let select = $(`select#${id}-version-select`);
     const currentVal = select.val();
     resetInputElement(select);
+    var valueName = (serviceInfo.JupyterLab.options[value] || {}).name || "new-jupyterlab";
     for (const service of Object.keys(dropdownOptions).sort().reverse()) {
       var serviceName = (serviceInfo.JupyterLab.options[service] || {}).name || service;
-      select.append(`<option value="${service}">${serviceName}</option>`);
+      // When filling the service dropdown, we only add deprecated versions,
+      // if the current value is already deprecated
+      if ( valueName.includes("deprecated") || ! serviceName.includes("deprecated")) {
+        select.append(`<option value="${service}">${serviceName}</option>`);
+      }
     }
     if (!value) value = serviceInfo.JupyterLab.defaultOption;
     updateLabConfigSelect(select, value, currentVal);
