778599ce4a3cee6e2e236a4877bb04b3dd8df44a
Maria Petrova <m.petrova@fz-juelich.de>
2024-07-04 17:48:10 +0200

    Create a POST request upon share and display the link

--- static/js/home/handle-servers.js
+++ static/js/home/handle-servers.js
@@ -206,15 +206,16 @@ require(["jquery", "jhapi", "utils", "home/utils", "home/lab-configs"], function
 
   function shareLab() {
 
-    function _uuidv4hex() {
-      return ([1e7, 1e3, 4e3, 8e3, 1e11].join('')).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+    function _uuid8digits() {
+      const randomInt = crypto.getRandomValues(new Uint32Array(1))[0];
+      // Convert the integer to a hexadecimal string and pad with zeros if necessary
+      const uuid = randomInt.toString(16).padStart(8, '0');
+      
+      return uuid.slice(0, 8);
     }
 
-    const uuid = _uuidv4hex();
-    var shareUrluuid = utils.url_path_join(jhdata.base_url, uuid);
-    console.log(`Sharing uuid URL: ${shareUrluuid}`)
-
+    const share_uuid = _uuid8digits();
+    console.log(`Sharing uuid URL: ${share_uuid}`)
 
     // Start button is in collapsible tr for new labs
     var [collapsibleTr, id] = _getTrAndId(this);
@@ -222,14 +223,46 @@ require(["jquery", "jhapi", "utils", "home/utils", "home/lab-configs"], function
 
     var options = _createDataDict(collapsibleTr);
 
-    var data = JSON.stringify(options);
-    var shareUrlOptions = utils.url_path_join(base_url, "share?");
-    for(var name in data) {
-      shareUrlOptions = utils.url_path_join(shareUrlOptions, "&", name, "=", data[name]);
-    }
 
-    alert(`Sharing uuid URL: ${shareUrlOptions}`);
-
+    //---------------------------------------------------
+  
+    
+      // Open a new tab for spawn_pending.html
+      // Need to create it here for JS context reasons
+      // var newTab = window.open("about:blank");
+      let protocol = window.location.protocol;
+      let urlStr = utils.url_path_join(protocol, getHostname(), base_url, "share", share_uuid);
+      api.share_server(share_uuid, {
+        data: JSON.stringify(options),
+        success: function () {
+          // url = utils.url_path_join(base_url, "share", share_uuid);
+          // newTab.location.href = url;
+          
+          // Reload page to add spawner to table
+          // location.reload();
+        },
+        error: function (xhr) {
+          // newTab.close();
+          // If cookie is not valid anymore, refresh the page.
+          // This should redirect the user to the login page.
+          if (xhr.status == 403) {
+            document.location.reload();
+            return;
+          }
+          // Show information about why the start failed
+          let details = $("<details>")
+            .append($("<summary>")
+              .html(`Failed to create a share link for this lab. Error: ${xhr.responseText}`))
+            .append($("<pre>")
+              .html(custom_utils.parseJSON(xhr.responseText)));
+          $(`#${id}-log`).append(
+            $("<div>").addClass("log-div").html(details)
+          );
+          collapsibleTr.find("button").removeClass("disabled");
+        }
+      });
+      let url = new URL(urlStr)
+      alert(`Sharing uuid URL: ${url}`);
   }
   
   $(".btn-start-lab").click(startServer);
--- static/js/jhapi.js
+++ static/js/jhapi.js
@@ -83,6 +83,15 @@ define(["jquery", "utils"], function ($, utils) {
     );
   };
 
+  JHAPI.prototype.share_server = function (uuid, options) {
+    options = options || {};
+    options = update(options, { type: "POST", dataType: null });
+    this.api_request(
+      utils.url_path_join("share", uuid),
+      options
+    );
+  };
+
   JHAPI.prototype.stop_server = function (user, options) {
     options = options || {};
     options = update(options, { type: "DELETE", dataType: null });
