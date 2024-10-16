d9a34d3f6ac10dff137756b7a8d76eab4252d726
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2024-10-16 10:05:19 +0200

    support default value in service specific default values

--- static/js/home/dropdown-options.js
+++ static/js/home/dropdown-options.js
@@ -380,7 +380,9 @@ define(["jquery", "home/utils"], function (
                   var checked = moduleInfo.default ? "checked" : "";
                 } else if ( typeof moduleInfo.default == "object" && service in moduleInfo.default ) {
                   var checked = ( moduleInfo.default[service] || false) ? "checked" : "";
-                } else {
+                } else if ( typeof moduleInfo.default == "object" && "default" in moduleInfo.default ) {
+                  var checked = moduleInfo.default.default ? "checked" : "";
+		} else {
                   var checked = "";
                 }
                 let module_cols = "col-sm-6 col-md-4 col-lg-3";
